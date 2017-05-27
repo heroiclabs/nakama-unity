c910a9800b1143b498ffb6019a783ef32dfe8be1
Mo Firouz <mo@heroiclabs.com>
2017-05-27 16:51:39 +0100

    Change INMessage to INCollatedMessage for RPC messages

--- Nakama/NRuntimeRpcMessage.cs
+++ Nakama/NRuntimeRpcMessage.cs
@@ -20,7 +20,7 @@ using Google.Protobuf;
 
 namespace Nakama
 {
-    public class NRuntimeRpcMessage : INMessage<INRuntimeRpc>
+    public class NRuntimeRpcMessage : INCollatedMessage<INRuntimeRpc>
     {
         private Envelope payload;
         public IMessage Payload {
