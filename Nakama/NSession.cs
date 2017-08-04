8d824d5f00a015e889892a41ae574ba566e492c4
Tim Aksu <timur.s.aksu@gmail.com>
2017-08-04 11:20:30 +1000

    Fixed issue where NSession was not calculating expiry time corretly

--- Nakama/NSession.cs
+++ Nakama/NSession.cs
@@ -21,6 +21,8 @@ namespace Nakama
 {
     public class NSession : INSession
     {
+        private static readonly DateTime Epoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
         public long CreatedAt { get; private set; }
 
         public long ExpiresAt { get; private set; }
@@ -52,12 +54,13 @@ namespace Nakama
 
         public bool HasExpired(DateTime dateTime)
         {
-            return (ExpiresAt - TimeSpan.FromTicks(dateTime.Ticks).TotalMilliseconds) < 0L;
+            var expireDate = Epoch + TimeSpan.FromMilliseconds(ExpiresAt);
+            return dateTime > expireDate;
         }
 
         public static INSession Restore(string token)
         {
-            TimeSpan span = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            TimeSpan span = DateTime.UtcNow - Epoch;
             return new NSession(token, System.Convert.ToInt64(span.TotalMilliseconds));
         }
 
