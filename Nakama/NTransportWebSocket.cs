1b329e9be0230d38773a25943f03ac7664fa1472
Steve Streeting <steve@stevestreeting.com>
2018-03-21 10:03:07 +0000

    Fix translation of auth errors; blanket successAction is in fact needed
    Original fix will still work since parse errors are now caught

--- Nakama/NTransportWebSocket.cs
+++ Nakama/NTransportWebSocket.cs
@@ -112,6 +112,11 @@ namespace Nakama
                     }
                     catch (WebException e)
                     {
+                        if (e.Response is HttpWebResponse)
+                        {
+                            successAction(e.Response as HttpWebResponse);
+                            return;
+                        }
                         errorAction(e);
                     }
                 }, request);
