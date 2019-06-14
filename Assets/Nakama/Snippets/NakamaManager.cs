221ea7c4c306b193c03ef8b2b5fd8629ed81a80c
Chris Molozian <chris@heroiclabs.com>
2019-06-14 11:41:03 +0100

    Add example on how to manage the client sdk as a Unity singleton.

---
+++ Assets/Nakama/Snippets/NakamaManager.cs
@@ -0,0 +1,117 @@
+/**
+ * Copyright 2019 The Nakama Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Nakama.Snippets
+{
+    /// <summary>
+    /// Manages a Nakama client and optional socket connection.
+    /// </summary>
+    /// <seealso cref="NakamaManagerUsage"/>
+    public class NakamaManager : MonoBehaviour
+    {
+        private const string SessionPrefName = "nakama.session";
+        private const string SingletonName = "/[NakamaManager]";
+
+        private static readonly object Lock = new object();
+        private static NakamaManager _instance;
+
+        /// <summary>
+        /// The singleton instance of the Nakama sdk manager.
+        /// </summary>
+        public static NakamaManager Instance
+        {
+            get
+            {
+                lock (Lock)
+                {
+                    if (_instance != null) return _instance;
+                    var go = GameObject.Find(SingletonName);
+                    if (go == null)
+                    {
+                        go = new GameObject(SingletonName);
+                    }
+
+                    if (go.GetComponent<NakamaManager>() == null)
+                    {
+                        go.AddComponent<NakamaManager>();
+                    }
+                    DontDestroyOnLoad(go);
+                    _instance = go.GetComponent<NakamaManager>();
+                    return _instance;
+                }
+            }
+        }
+
+        public IClient Client { get; }
+        public ISocket Socket { get; }
+
+        public Task<ISession> Session { get; private set; }
+
+        private NakamaManager()
+        {
+            Client = new Client("http", "127.0.0.1", 7350, "defaultkey")
+            {
+#if UNITY_EDITOR
+                Logger = new UnityLogger()
+#endif
+            };
+            Socket = Client.NewSocket();
+        }
+
+        private Task<ISession> AuthenticateAsync()
+        {
+            // Modify to fit the authentication strategy you want within your game.
+            // EXAMPLE:
+            const string deviceIdPrefName = "deviceid";
+            var deviceId = PlayerPrefs.GetString(deviceIdPrefName,SystemInfo.deviceUniqueIdentifier);
+#if UNITY_EDITOR
+            Debug.LogFormat("Device id: {0}", deviceId);
+#endif
+            // With device IDs save it locally in case of OS updates which can change the value on device.
+            PlayerPrefs.SetString(deviceIdPrefName, deviceId);
+            return Client.AuthenticateDeviceAsync(deviceId);
+        }
+
+        private void Awake()
+        {
+            // Restore session or create a new one.
+            var authToken = PlayerPrefs.GetString(SessionPrefName);
+            var expiredDate = DateTime.UtcNow.AddDays(-1);
+            if (string.IsNullOrEmpty(authToken) || Nakama.Session.Restore(authToken).HasExpired(expiredDate))
+            {
+                var sessionTask = AuthenticateAsync();
+                Session = sessionTask;
+                sessionTask.ContinueWith(t =>
+                {
+                    if (t.IsCompleted)
+                    {
+                        PlayerPrefs.SetString(SessionPrefName, t.Result.AuthToken);
+                    }
+                }, TaskScheduler.FromCurrentSynchronizationContext());
+            }
+            else
+            {
+                Session = Task.FromResult(Nakama.Session.Restore(authToken));
+            }
+        }
+
+        private void OnApplicationQuit() => Socket?.CloseAsync();
+    }
+}
---
+++ Assets/Nakama/Snippets/NakamaManagerUsage.cs
@@ -0,0 +1,36 @@
+/**
+ * Copyright 2019 The Nakama Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using UnityEngine;
+
+namespace Nakama.Snippets
+{
+    /// <seealso cref="NakamaManager"/>
+    public class NakamaManagerUsage : MonoBehaviour
+    {
+        private async void Start()
+        {
+            var session = await NakamaManager.Instance.Session;
+            Debug.LogFormat("Active Session: {0}", session);
+            var account = await NakamaManager.Instance.Client.GetAccountAsync(session);
+            Debug.LogFormat("Account id: {0}", account.User.Id);
+
+            NakamaManager.Instance.Socket.Closed += () => Debug.Log("Socket closed.");
+            NakamaManager.Instance.Socket.Connected += () => Debug.Log("Socket connected.");
+            await NakamaManager.Instance.Socket.ConnectAsync(session);
+        }
+    }
+}
